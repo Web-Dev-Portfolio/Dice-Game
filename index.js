007665c3b4b12cc49a397e641670f5566a8e7be3
fitransyah-sourcetree <fitransyah.rusman@gmail.com>
2022-05-05 16:32:09 +0700

    refactoring for dice code

--- index.js
+++ index.js
@@ -1,57 +1,15 @@
-var randomNumber1 = Math.floor(Math.random()*6+1);
-var randomNumber2 = Math.floor(Math.random()*6+1);
-switch(randomNumber1)
-{
-  case 1:
-  document.querySelector("img.img1").setAttribute("src", "images/dice1.png");
-  break;
-  case 2:
-  document.querySelector("img.img1").setAttribute("src", "images/dice2.png");
-  break;
-  case 3:
-  document.querySelector("img.img1").setAttribute("src", "images/dice3.png");
-  break;
-  case 4:
-  document.querySelector("img.img1").setAttribute("src", "images/dice4.png");
-  break;
-  case 5:
-  document.querySelector("img.img1").setAttribute("src", "images/dice5.png");
-  break;
-  case 6:
-  document.querySelector("img.img1").setAttribute("src", "images/dice6.png");
-  break;
-}
-switch(randomNumber2)
-{
-  case 1:
-  document.querySelector("img.img2").setAttribute("src", "images/dice1.png");
-  break;
-  case 2:
-  document.querySelector("img.img2").setAttribute("src", "images/dice2.png");
-  break;
-  case 3:
-  document.querySelector("img.img2").setAttribute("src", "images/dice3.png");
-  break;
-  case 4:
-  document.querySelector("img.img2").setAttribute("src", "images/dice4.png");
-  break;
-  case 5:
-  document.querySelector("img.img2").setAttribute("src", "images/dice5.png");
-  break;
-  case 6:
-  document.querySelector("img.img2").setAttribute("src", "images/dice6.png");
-  break;
-}
+var randomNumber1 = Math.floor(Math.random() * 6 + 1);
+var randomDice1 = "images/dice" + randomNumber1 + ".png";
+document.querySelector("img.img1").setAttribute("src", randomDice1);
 
-if (randomNumber1 > randomNumber2)
-{
+var randomNumber2 = Math.floor(Math.random() * 6 + 1);
+var randomDice2 = "images/dice"+ randomNumber2 +".png";
+document.querySelector("img.img2").setAttribute("src", randomDice2);
+
+if (randomNumber1 > randomNumber2) {
   document.querySelector("h1#status").textContent = "🚩 Player 1 Wins!";
-}
-else if (randomNumber2 > randomNumber1)
-{
+} else if (randomNumber2 > randomNumber1) {
   document.querySelector("h1#status").textContent = "Player 2 Wins! 🚩";
-}
-else if (randomNumber1 === randomNumber2)
-{
+} else if (randomNumber1 === randomNumber2) {
   document.querySelector("h1#status").textContent = "It's a Draw!";
 }
